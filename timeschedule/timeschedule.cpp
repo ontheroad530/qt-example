e4b44c029161ec14942d843573418bce5c384543
ontheroad530 <ontheroad530@163.com>
2024-09-18 13:40:44 +0800

    更新TimeSchedule

--- timeschedule/timeschedule.cpp
+++ timeschedule/timeschedule.cpp
@@ -1,12 +1,14 @@
 #include "timeschedule.h"
 
+#include <QMenu>
 #include <QDebug>
 #include <QPainter>
 #include <QMouseEvent>
 
 #define BOUNDARY_ERR 5
-#define CANVAS_BOUND 20
 #define SCALE_COUNT 24
+
+#define SCALE_ACCURACY (60*24)
 
 Range::Range()
 {
@@ -96,9 +98,14 @@ void TimeSchedule::setBarColor(const QColor& color)
 	m_barColor = color;
 }
 
-void TimeSchedule::setFontColor(const QColor& color)
+void TimeSchedule::setBackgroundColor(const QColor& color)
 {
-	m_fontColor = color;
+	m_bgColor = color;
+}
+
+void TimeSchedule::setForegroundColor(const QColor& color)
+{
+	m_fgColor = color;
 }
 
 void TimeSchedule::setMainScaleMarkLenth(int len)
@@ -111,30 +118,52 @@ void TimeSchedule::setSubScaleMarkLenth(int len)
 	m_subScaleMarkLen = len;
 }
 
+void TimeSchedule::addRange(int min, int max)
+{
+	m_ranges << Range(min, max);
+
+	if (this->isVisible())
+	{
+		update();
+	}
+}
+
+void TimeSchedule::clear()
+{
+	m_ranges.clear();
+	update();
+}
+
+QVector<QPair<int, int>> TimeSchedule::getTimeDurations()
+{
+	QVector<QPair<int, int>> timeDurations;
+	
+	for (const auto& range : m_ranges)
+	{
+		int min = range.min();
+		int max = range.max();
+		timeDurations << qMakePair<int, int>(min, max);
+	}
+
+	return timeDurations;
+}
+
 void TimeSchedule::paintEvent(QPaintEvent* event)
 {
 	QPainter painter(this);
 
+	drawBackground(&painter);
 	drawTimeBar(&painter);
 	drawTimeRuler(&painter);
 	drawOpRangeText(&painter);
 }
 
-void TimeSchedule::resizeEvent(QResizeEvent* event)
+void TimeSchedule::leaveEvent(QEvent* event)
 {
-	int oldBoundary = getBoundary(event->oldSize().width());
-	int newBoundary = getBoundary(event->size().width());
+	m_rangeText.clear();
+	update();
 
-	int oldWidth = event->oldSize().width() - 2 * oldBoundary;
-	int newWidth = event->size().width() - 2 * newBoundary;
-
-	for (auto& range : m_ranges)
-	{
-		range.setMin((range.min() - oldBoundary) * newWidth / oldWidth + newBoundary);
-		range.setMax((range.max()- oldBoundary)  * newWidth / oldWidth + newBoundary);
-	}
-
-	QWidget::resizeEvent(event);
+	QWidget::leaveEvent(event);
 }
 
 void TimeSchedule::mousePressEvent(QMouseEvent* event)
@@ -143,11 +172,14 @@ void TimeSchedule::mousePressEvent(QMouseEvent* event)
 	if (posX >= canvasBeginPos() && posX <= canvasEndPos())
 	{
 		m_opType = OP_DRAW;
-		m_pressedPosX = posX;
-		m_lastPosX = m_pressedPosX;
+
+		int time = position2TimeValue(posX);
+		m_opRange = Range(time, time);
+		m_pressedTimeX = time;
+		m_lastTimeX = m_pressedTimeX;
 		for (int i = 0; i < m_ranges.size(); ++i)
 		{
-			if (m_ranges[i].isToMaxDiff(posX) < BOUNDARY_ERR)
+			if (m_ranges[i].isToMaxDiff(time) <= BOUNDARY_ERR)
 			{
 				m_opRange = m_ranges[i];
 				m_opType = OP_RESIZE_MAX;
@@ -155,7 +187,7 @@ void TimeSchedule::mousePressEvent(QMouseEvent* event)
 				this->setCursor(Qt::SizeHorCursor);
 				break;
 			}
-			else if (m_ranges[i].isToMinDiff(posX) < BOUNDARY_ERR)
+			else if (m_ranges[i].isToMinDiff(time) <= BOUNDARY_ERR)
 			{
 				m_opRange = m_ranges[i];
 				m_opType = OP_RESIZE_MIN;
@@ -163,7 +195,7 @@ void TimeSchedule::mousePressEvent(QMouseEvent* event)
 				this->setCursor(Qt::SizeHorCursor);
 				break;
 			}
-			else if (m_ranges[i].contains(posX))
+			else if (m_ranges[i].contains(time))
 			{
 				m_opRange = m_ranges[i];
 				m_opType = OP_SELECT;
@@ -180,11 +212,9 @@ void TimeSchedule::mousePressEvent(QMouseEvent* event)
 
 void TimeSchedule::mouseReleaseEvent(QMouseEvent* event)
 {
-	m_ranges << m_opRange;
+	addNewRange(m_opRange);
 	m_opType = OP_NONE;
 	this->unsetCursor();
-
-	mergeRanges();
 	update();
 
 	QWidget::mouseReleaseEvent(event);
@@ -195,34 +225,39 @@ void TimeSchedule::mouseMoveEvent(QMouseEvent* event)
 	int posX = event->pos().x();
 	if (posX >= canvasBeginPos() && posX <= canvasEndPos())
 	{
+		int time = position2TimeValue(posX);
 		if (OP_DRAW == m_opType)
 		{
-			m_opRange.setMin(m_pressedPosX);
-			m_opRange.setMax(qMax(m_pressedPosX, posX));
+			m_opRange.setMin(m_pressedTimeX);
+			m_opRange.setMax(qMax(m_pressedTimeX, time));
 			update();
 		}
 		else if (OP_SELECT == m_opType)
 		{
-			int move = posX - m_lastPosX;
+			int move = time - m_lastTimeX;
 			int min = m_opRange.min() + move;
 			int max = m_opRange.max() + move;
-			if (min >= canvasBeginPos() && max <= canvasEndPos())
+
+
+			if (min >= 0 && max <= SCALE_ACCURACY)
 			{
 				m_opRange.setMin(min);
 				m_opRange.setMax(max);
 			}
 
-			m_lastPosX = posX;
+			m_lastTimeX = time;
 			update();
 		}
 		else if (OP_RESIZE_MIN == m_opType)
 		{
-			m_opRange.setMin(qMin(posX, m_opRange.max()));
+			m_opRange.setMin(qMin(time, m_opRange.max()));
+			this->setCursor(Qt::SizeHorCursor);
 			update();
 		}
 		else if (OP_RESIZE_MAX == m_opType)
 		{
-			m_opRange.setMax(qMax(posX, m_opRange.min()));
+			m_opRange.setMax(qMax(time, m_opRange.min()));
+			this->setCursor(Qt::SizeHorCursor);
 			update();
 		}
 		else if (OP_NONE == m_opType)
@@ -230,11 +265,10 @@ void TimeSchedule::mouseMoveEvent(QMouseEvent* event)
 			bool showSizeCursor = false;
 			for (int i = 0; i < m_ranges.size(); ++i)
 			{
-				if (m_ranges[i].isToMaxDiff(posX) < BOUNDARY_ERR
-					|| m_ranges[i].isToMinDiff(posX) < BOUNDARY_ERR)
+				if (m_ranges[i].isToMaxDiff(time) <= BOUNDARY_ERR
+					|| m_ranges[i].isToMinDiff(time) <= BOUNDARY_ERR)
 				{
 					showSizeCursor = true;
-					
 					break;
 				}
 			}
@@ -245,29 +279,50 @@ void TimeSchedule::mouseMoveEvent(QMouseEvent* event)
 				this->unsetCursor();
 			}
 		}
+
+		updateHoverRangeText(time);
+	}
+	else
+	{
+		this->unsetCursor();
 	}
 	
 	QWidget::mouseMoveEvent(event);
+}
+
+void TimeSchedule::drawBackground(QPainter* painter)
+{
+	painter->save();
+	painter->setPen(Qt::NoPen);
+	painter->setBrush(m_bgColor);
+
+	QRect r = rect();
+	r.setLeft(canvasBeginPos());
+	r.setRight(canvasEndPos());
+	painter->drawRect(r);
+	painter->restore();
 }
 
 void TimeSchedule::drawTimeRuler(QPainter* painter)
 {
 	int numScales = SCALE_COUNT;
 	painter->save();
-	painter->setPen(m_fontColor);
+	painter->setPen(m_fgColor);
 
 	for (int i = 0; i < numScales+1; ++i)
 	{
 		int posX = canvasBeginPos() + this->canvasLength() / numScales * i;
 		if (i % 2 == 0){
-			painter->drawLine(posX, 0, posX, m_mainScaleMarkLen);
+			int posY = this->height() - m_mainScaleMarkLen;
+			painter->drawLine(posX, posY, posX, this->height());
 
 			QString text = QString("%1:00").arg(i, 2, 10, QChar('0'));
 			int width = this->fontMetrics().width(text);
-			int height = this->fontMetrics().height() + m_mainScaleMarkLen;
+			int height = this->height() - m_mainScaleMarkLen;
 			painter->drawText(posX-width/2, height, text);
 		}else{
-			painter->drawLine(posX, 0, posX, m_subScaleMarkLen);
+			int posY = this->height() - m_subScaleMarkLen;
+			painter->drawLine(posX, posY, posX, this->height());
 		}
 	}
 
@@ -283,12 +338,29 @@ void TimeSchedule::drawTimeBar(QPainter* painter)
 	//绘制未改变的区域
 	for (const auto& range : m_ranges)
 	{
-		painter->drawRect(range.min(), 0, range.rangeValue(), this->height());
+		int x1 = timeValue2Position(range.min());
+		int x2 = timeValue2Position(range.max());
+
+		QRect rect;
+		rect.setLeft(x1);
+		rect.setRight(x2);
+		rect.setTop(0);
+		rect.setBottom(this->height());
+		painter->drawRect(rect);
 	}
 
 	if (OP_NONE != m_opType)
 	{
-		painter->drawRect(m_opRange.min(), 0, m_opRange.rangeValue(), this->height());
+		int x1 = timeValue2Position(m_opRange.min());
+		int x2 = timeValue2Position(m_opRange.max());
+
+		QRect rect;
+		rect.setLeft(x1);
+		rect.setRight(x2);
+		rect.setTop(0);
+		rect.setBottom(this->height());
+
+		painter->drawRect(rect);
 	}
 
 	painter->restore();
@@ -296,91 +368,162 @@ void TimeSchedule::drawTimeBar(QPainter* painter)
 
 void TimeSchedule::drawOpRangeText(QPainter* painter)
 {
-	if (OP_NONE == m_opType){
+	if (m_rangeText.isEmpty()){
 		return;
 	}
 
-	int minValue = m_opRange.min() - canvasBeginPos();
-	int maxValue = m_opRange.max() - canvasBeginPos();
-	int scacleInterval = (qreal)canvasLength() / (qreal)SCALE_COUNT;
+	int fontWidth = this->fontMetrics().width(m_rangeText);
+	int fontHeight = this->fontMetrics().height();
 
-	QString beginTime = QString("%1:%2")
-		.arg(minValue / scacleInterval, 2, 10, QChar('0'))
-		.arg(minValue % scacleInterval * 60 / scacleInterval, 2, 10, QChar('0'));
+	int posX = timeValue2Position(m_hoverRange.min());
+	int pixels = timeValue2Pixels(m_hoverRange.rangeValue());
 
-	QString endTime = QString("%1:%2")
-		.arg(maxValue / scacleInterval, 2, 10, QChar('0'))
-		.arg(maxValue % scacleInterval * 60 / scacleInterval, 2, 10, QChar('0'));
-
-	QString text = QString("%1--%2").arg(beginTime).arg(endTime);
-
-
-	int fontWidth = this->fontMetrics().width(text);
-
-	int x = canvasBeginPos() + (canvasLength() + fontWidth)/ 2 ;
-	int y = this->height() - 5;
+	int x = posX + pixels / 2 - fontWidth / 2;
+	int y = fontHeight;
 
 	painter->save();
-	painter->setPen(m_fontColor);
-	painter->drawText(x, y, text);
+	painter->setPen(m_fgColor);
+	painter->drawText(x, y, m_rangeText);
 	painter->restore();
 }
 
-void TimeSchedule::mergeRanges()
+void TimeSchedule::updateHoverRangeText(int time)
 {
-	//去重
-	QSet<int> valueSet;
-	for (const auto& range : m_ranges)
+	bool showText = false;
+	m_rangeText.clear();
+	if (OP_NONE == m_opType)
 	{
-		valueSet << range.min();
-		valueSet << range.max();
+		for (int i = 0; i < m_ranges.size(); ++i)
+		{
+			if (m_ranges[i].contains(time))
+			{
+				showText = true;
+				m_hoverRange = m_ranges[i];
+				break;
+			}
+		}
+	}
+	else
+	{
+		m_hoverRange = m_opRange;
+		showText = true;
 	}
 
-	//排序
-	QList<int> list = valueSet.toList();
-	qSort(list);
-
-	//合并
-	QVector<Range> newRanges;
-	for (int i = 0; i < m_ranges.size(); ++i)
+	if (showText)
 	{
-		if (i + 1 >= m_ranges.size())
+		m_rangeText = range2TimeText(m_hoverRange);
+	}
+
+	update();
+}
+
+void TimeSchedule::addNewRange(const Range& range)
+{
+	Range r = range;
+	for (auto iter = m_ranges.begin(); iter != m_ranges.end();)
+	{
+		if (r.intersected(*iter))
 		{
-			newRanges << m_ranges[i];
-			break;
+			r = r.united(*iter);
+			iter = m_ranges.erase(iter);
 		}
 		else
 		{
-			if (m_ranges[i].intersected(m_ranges[i + 1]))
-			{
-				m_ranges[i + 1] = m_ranges[i].united(m_ranges[i + 1]);
-			}
-			else
-			{
-				newRanges << m_ranges[i];
-			}
+			++iter;
 		}
 	}
 
-	m_ranges = newRanges;
+	m_ranges << r;
+
+	//去除无效段
+	for (auto iter = m_ranges.begin(); iter != m_ranges.end();)
+	{
+		if (iter->rangeValue() <= 0) {
+			iter = m_ranges.erase(iter);
+		}else {
+			++iter;
+		}
+	}
 }
 
-int TimeSchedule::getBoundary(int w)
+int TimeSchedule::getLeftBoundary(int w)
 {
-	return w % SCALE_COUNT + CANVAS_BOUND;
+	return w % SCALE_COUNT + SCALE_COUNT;
+}
+
+int TimeSchedule::getRightBoundary(int w)
+{
+	return  SCALE_COUNT;
 }
 
 int TimeSchedule::canvasBeginPos()
 {
-	return getBoundary(this->width());
+	return getLeftBoundary(this->width());
 }
 
 int TimeSchedule::canvasEndPos()
 {
-	return this->width() - getBoundary(this->width());
+	return this->width() - getRightBoundary(this->width());
 }
 
 int TimeSchedule::canvasLength()
 {
-	return this->width() - 2 * getBoundary(this->width());
+	return this->width() - getLeftBoundary(this->width()) - getRightBoundary(this->width());
+}
+
+void TimeSchedule::range2Time(const Range& range, int& beginHour, int& beginMinue, int& endHour, int& endMinute)
+{
+	beginHour = range.min() / 60;
+	beginMinue = range.min() % 60;
+
+	endHour = range.max() / 60;
+	endMinute = range.max() % 60;
+}
+
+void TimeSchedule::time2Range(int beginHour, int beginMinue, int endHour, int endMinute, Range& range)
+{
+	range.setMin( beginHour * 60 + beginMinue );
+	range.setMax( endHour * 60 + endMinute );
+}
+
+QString TimeSchedule::range2TimeText(const Range& range)
+{
+	int beginHour, beginMinue, endHour, endMinute;
+	range2Time(range, beginHour, beginMinue, endHour, endMinute);
+	return QString("%1:%2-%3:%4")
+		.arg(beginHour, 2, 10, QChar('0')).arg(beginMinue, 2, 10, QChar('0'))
+		.arg(endHour, 2, 10, QChar('0')).arg(endMinute, 2, 10, QChar('0'));
+}
+
+int TimeSchedule::position2TimeValue(int pos)
+{
+	int value = pos - canvasBeginPos();
+	int hourPixels = canvasLength() / SCALE_COUNT;
+
+	int hour = value / hourPixels;
+	int minute = value % hourPixels * 60 / hourPixels;
+
+	return hour * 60 + minute;
+}
+
+int TimeSchedule::timeValue2Position(int minute)
+{
+	return timeValue2Pixels(minute) + canvasBeginPos();
+}
+
+int TimeSchedule::timeValue2Position(int hour, int minute)
+{
+	return timeValue2Pixels(hour, minute) + canvasBeginPos();
+}
+
+int TimeSchedule::timeValue2Pixels(int minute)
+{
+	return timeValue2Pixels(minute / 60, minute % 60);
+}
+
+int TimeSchedule::timeValue2Pixels(int hour, int minute)
+{
+	int hourPixels = canvasLength() / SCALE_COUNT;
+	int pixels = hour * hourPixels + minute * hourPixels / 60;
+	return pixels;
 }
--- timeschedule/timeschedule.h
+++ timeschedule/timeschedule.h
@@ -55,44 +55,74 @@ public:
     ~TimeSchedule();
 
 	void setBarColor(const QColor& color);
-	void setFontColor(const QColor& color);
+	void setBackgroundColor(const QColor& color);
+	void setForegroundColor(const QColor& color);
 
 	void setMainScaleMarkLenth(int len);
 	void setSubScaleMarkLenth(int len);
 
+	//0~24*60
+	void addRange(int min, int max);
+	void clear();
+
+	QVector<QPair<int, int>> getTimeDurations();
+
 protected:
 	void paintEvent(QPaintEvent* event) Q_DECL_OVERRIDE;
-	void resizeEvent(QResizeEvent* event) Q_DECL_OVERRIDE;
+	void leaveEvent(QEvent* event) Q_DECL_OVERRIDE;
 
 	void mousePressEvent(QMouseEvent* event) Q_DECL_OVERRIDE;
 	void mouseReleaseEvent(QMouseEvent* event) Q_DECL_OVERRIDE;
 	void mouseMoveEvent(QMouseEvent* event) Q_DECL_OVERRIDE;
 
 private:
+	void drawBackground(QPainter* painter);
 	void drawTimeRuler(QPainter* painter);
 	void drawTimeBar(QPainter* painter);
 	void drawOpRangeText(QPainter* painter);
 
-	void mergeRanges();
+	void updateHoverRangeText(int posX);
 
-	int getBoundary(int w);
+	void addNewRange(const Range& range);
+
+	int getLeftBoundary(int w);
+	int getRightBoundary(int w);
 	int canvasBeginPos();
 	int canvasEndPos();
 	int canvasLength();
+
+	void range2Time(const Range& range, int& beginHour, int& beginMinue, int& endHour, int& endMinute);
+	void time2Range(int beginHour, int beginMinue, int endHour, int endMinute, Range& range);
+	QString range2TimeText(const Range& range);
+
+	//
+	int position2TimeValue(int pos);
+	int timeValue2Position(int minute);
+	int timeValue2Position(int hour, int minute);
+
+	int timeValue2Pixels(int minute);
+	int timeValue2Pixels(int hour, int minute);
 
 private:
     Ui::TimeScheduleClass *ui;
 
 	QColor			m_barColor;
-	QColor			m_fontColor;
+	QColor			m_fgColor;
+	QColor			m_bgColor;
 
 	int				m_mainScaleMarkLen = 10;
 	int				m_subScaleMarkLen = 5;
 
-	int				m_pressedPosX = 0;
-	int				m_lastPosX = 0;
+	//int				m_pressedPosX = 0;
+	//int				m_lastPosX = 0;
+
+	int				m_pressedTimeX = 0;
+	int				m_lastTimeX = 0;
 
 	OpType			m_opType = OP_NONE;
 	Range			m_opRange;
+	
+	QString			m_rangeText;
+	Range			m_hoverRange;
 	QVector<Range>	m_ranges;
 };
